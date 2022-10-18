8a6e028a98517756879460769613d587c862b30a
Marjo Forcado <marjoforcado@gmail.com>
2022-10-18 16:27:51 +0800

    Add redux toolkit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
 import "../styles/globals.scss";
+
+import { Provider } from "react-redux";
 import type { AppProps } from "next/app";
 
+import store from "../store";
+
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
 export default MyApp;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,18 @@
 import type { NextPage } from "next";
+import { useAppDispatch, useAppSelector } from "../store/hooks";
+
+import { increment } from "../store/counter/slice";
 
 const Home: NextPage = () => {
-  return <div>here</div>;
+  const count = useAppSelector((state) => state.counter.value);
+  const dispatch = useAppDispatch();
+
+  return (
+    <>
+      <div>{count}</div>
+      <button onClick={() => dispatch(increment())}>Click me</button>
+    </>
+  );
 };
 
 export default Home;
