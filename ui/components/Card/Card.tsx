7a778a8787e2bf929ae83be6bee66918cb1888ca
Marjo Forcado <marjoforcado@gmail.com>
2022-10-19 20:31:46 +0800

    Update ui

--- ui/components/Card/Card.tsx
+++ ui/components/Card/Card.tsx
@@ -1,26 +1,16 @@
-import classNames from "classnames";
-import Image from "next/image";
 import Text from "../Text";
-
 import styles from "./styles.module.scss";
 
-type PropsType = {
-  isLoading?: boolean;
-};
-
-const Card = (props: PropsType) => {
-  return (
-    <div className={styles["card"]}>
-      <div className={styles["card__media"]}>{/* <Image /> */}</div>
-      <div className={styles["card__body"]}>
-        <Text size="lg">Title</Text>
-        <Text size="sm">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum,
-          minus.
-        </Text>
-      </div>
+const Card = () => (
+  <div className={styles["card"]}>
+    <div className={styles["card__media"]}></div>
+    <div className={styles["card__body"]}>
+      <Text>Kohien (香妃園)</Text>
+      <Text size="sm">
+        六本木3丁目8-15 (瀬里奈ビレッジ 2F), 港区, Tokyo, 106-0032
+      </Text>
     </div>
-  );
-};
+  </div>
+);
 
 export default Card;
