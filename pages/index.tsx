f7a674cd5fffb4fdcacb4527f50ef8f4c44db176
Marjo Forcado <marjoforcado@gmail.com>
2022-10-20 00:16:56 +0800

    Add card props

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,19 +2,12 @@ import type { NextPage } from "next";
 import { useAppDispatch, useAppSelector } from "../store/hooks";
 
 import { useGetPlacesQuery } from "../store/api/slice";
-import { increment } from "../store/counter/slice";
 import { Button, Card, Input } from "../ui/components";
 
 import styles from "./styles.module.scss";
 
 const Home: NextPage = () => {
-  const count = useAppSelector((state) => state.counter.value);
   const { data, isFetching } = useGetPlacesQuery();
-
-  const dispatch = useAppDispatch();
-
-  console.log(isFetching);
-  console.log(data);
 
   return (
     <div className={styles["page"]}>
@@ -26,12 +19,14 @@ const Home: NextPage = () => {
         <Button>Search</Button>
       </div>
       <div className={styles["page__grid"]}>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        {data?.results.map((result) => (
+          <Card
+            key={result.fsq_id}
+            imageSrc={`${result.categories[0].icon.prefix}64${result.categories[0].icon.suffix}`}
+            title={result.name}
+            description={result.location.formatted_address}
+          />
+        ))}
       </div>
     </div>
   );
--- ui/components/Card/Card.tsx
+++ ui/components/Card/Card.tsx
@@ -1,16 +1,27 @@
+import Image from "next/image";
 import Text from "../Text";
 import styles from "./styles.module.scss";
 
-const Card = () => (
-  <div className={styles["card"]}>
-    <div className={styles["card__media"]}></div>
-    <div className={styles["card__body"]}>
-      <Text>Kohien (香妃園)</Text>
-      <Text size="sm">
-        六本木3丁目8-15 (瀬里奈ビレッジ 2F), 港区, Tokyo, 106-0032
-      </Text>
+type PropsType = {
+  imageSrc: string;
+  title: string;
+  description: string;
+};
+
+const Card = (props: PropsType) => {
+  const { imageSrc, title, description } = props;
+
+  return (
+    <div className={styles["card"]}>
+      <div className={styles["card__media"]}>
+        <Image src={imageSrc} alt="image" width={64} height={64} />
+      </div>
+      <div className={styles["card__body"]}>
+        <Text>{title}</Text>
+        <Text size="sm">{description}</Text>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Card;
