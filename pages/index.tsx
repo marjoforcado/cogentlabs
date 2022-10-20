278aff3842acc1f43dccec724dcc306b37a0ec6c
Marjo Forcado <marjoforcado@gmail.com>
2022-10-20 19:34:51 +0800

    Add view page

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,9 +5,10 @@ import { useGetPlacesQuery } from "../store/api/slice";
 import { Button, Card, Input } from "../ui/components";
 
 import styles from "./styles.module.scss";
+import Link from "next/link";
 
 const Home: NextPage = () => {
-  const { data, isFetching } = useGetPlacesQuery();
+  const { data } = useGetPlacesQuery();
 
   return (
     <div className={styles["page"]}>
@@ -20,12 +21,15 @@ const Home: NextPage = () => {
       </div>
       <div className={styles["page__grid"]}>
         {data?.results.map((result) => (
-          <Card
-            key={result.fsq_id}
-            imageSrc={`${result.categories[0].icon.prefix}64${result.categories[0].icon.suffix}`}
-            title={result.name}
-            description={result.location.formatted_address}
-          />
+          <Link key={result.fsq_id} href={`/view/${result.fsq_id}`}>
+            <a>
+              <Card
+                imageSrc={`${result.categories[0].icon.prefix}64${result.categories[0].icon.suffix}`}
+                title={result.name}
+                description={result.location.formatted_address}
+              />
+            </a>
+          </Link>
         ))}
       </div>
     </div>
---
+++ pages/view/[fsq_id].tsx
@@ -0,0 +1,5 @@
+import styles from "./styles.module.scss";
+
+const IndexPage = () => <div>View Page</div>;
+
+export default IndexPage;
