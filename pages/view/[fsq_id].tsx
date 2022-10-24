124dfcbe1dcfc8cc1a04c5400cbc535d54d550b5
Marjo Forcado <marjoforcado@gmail.com>
2022-10-24 20:55:13 +0800

    Refactor code

--- pages/view/[fsq_id].tsx
+++ pages/view/[fsq_id].tsx
@@ -1,22 +1,60 @@
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 import { useLazyGetPlacesDetailsQuery } from "../../store/api/slice";
+import { Container, Gallery, Text } from "../../ui/components";
+
+import styles from "./styles.module.scss";
 
 const IndexPage = () => {
   const { query, isReady } = useRouter();
   const [trigger, results] = useLazyGetPlacesDetailsQuery();
+  const { data, isFetching, isSuccess } = results;
 
   useEffect(() => {
     // Make sure query is available before requesting API.
     if (isReady) {
       trigger(query.fsq_id as string);
     }
+
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isReady]);
 
-  console.log(results);
+  if (isFetching) {
+    return <div>loading</div>;
+  }
 
-  return <div>here</div>;
+  if (isSuccess) {
+    const images = data.photos.map((photo: any) => ({
+      id: photo.id,
+      url: `${photo.prefix}original${photo.suffix}`,
+    }));
+
+    return (
+      <div className={styles["page"]}>
+        <Container centerContent>
+          <div className={styles["page__flex"]}>
+            <Gallery images={images} />
+            <div className={styles["page__container"]}>
+              <div className={styles["page__content"]}>
+                <Text size="2xl" weight="semibold">
+                  {data.details.name}
+                </Text>
+                <Text size="sm">{data.details.location.formatted_address}</Text>
+              </div>
+              <aside className={styles["page__aside"]}>
+                <iframe
+                  src={`https://maps.google.com/maps?q=${data.details.geocodes.main.latitude},${data.details.geocodes.main.longitude}&z=18&ie=UTF8&output=embed`}
+                  className={styles["page__map"]}
+                ></iframe>
+              </aside>
+            </div>
+          </div>
+        </Container>
+      </div>
+    );
+  }
+
+  return <></>;
 };
 
 export default IndexPage;
