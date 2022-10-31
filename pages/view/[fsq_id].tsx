8dc984ec9b7f7a98b594a100a82014c5e3b9035a
Marjo Forcado <marjoforcado@gmail.com>
2022-10-31 12:14:25 +0800

    Refactor code

--- pages/view/[fsq_id].tsx
+++ pages/view/[fsq_id].tsx
@@ -1,24 +1,25 @@
-import Link from "next/link";
+import classNames from "classnames";
 import { useRouter } from "next/router";
 import { useEffect } from "react";
-import { FixedSizeList } from "react-window";
 import { useLazyGetPlacesDetailsQuery } from "../../store/api/slice";
-import { CommentCard, Container, Gallery, Text } from "../../ui/components";
-import { Loader as GalleryLoader } from "../../ui/components/Gallery";
-
+import { Container, Gallery, Link, Text } from "../../ui/components";
 import styles from "./styles.module.scss";
 
 const IndexPage = () => {
   const { query, isReady } = useRouter();
-  const [trigger, { data, isFetching, isSuccess }] =
-    useLazyGetPlacesDetailsQuery();
+  const [getPlacesDetails, results] = useLazyGetPlacesDetailsQuery();
+  const { data, isFetching, isSuccess } = results;
+
+  const images = data?.photos.map((photo: any) => ({
+    id: photo.id,
+    url: `${photo.prefix}original${photo.suffix}`,
+  }));
 
   useEffect(() => {
     // Make sure query is available before requesting API.
     if (isReady) {
-      trigger(query.fsq_id as string, true);
+      getPlacesDetails(query.fsq_id as string, true);
     }
-
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isReady]);
 
@@ -26,76 +27,45 @@ const IndexPage = () => {
     <div className={styles["page"]}>
       <Container centerContent>
         <div className={styles["page__flex"]}>
-          <GalleryLoader />
+          <Gallery isLoading={isFetching} images={images} />
+          <div className={styles["page__container"]}>
+            <div className={styles["page__content"]}>
+              <div className={styles["page__header"]}>
+                <Link href="/">Go Back</Link>
+                {isFetching && (
+                  <div className={styles["page__loaders"]}>
+                    <div
+                      className={classNames(
+                        styles["page__loader"],
+                        styles["page__loader--title"]
+                      )}
+                    ></div>
+                    <div
+                      className={classNames(
+                        styles["page__loader"],
+                        styles["page__loader--address"]
+                      )}
+                    ></div>
+                  </div>
+                )}
+                {data?.details.name && (
+                  <Text size="2xl" weight="semibold">
+                    {data.details.name}
+                  </Text>
+                )}
+                {data?.details.location.formatted_address && (
+                  <Text size="sm">
+                    {data.details.location.formatted_address}
+                  </Text>
+                )}
+              </div>
+              <div className={styles["page__comments"]}>here</div>
+            </div>
+          </div>
         </div>
       </Container>
     </div>
   );
-
-  if (isFetching) {
-    return <div>loading</div>;
-  }
-
-  if (isSuccess) {
-    if (data.photos === undefined || data.tips === undefined) {
-      return <div>No data</div>;
-    }
-
-    const images = data.photos.map((photo: any) => ({
-      id: photo.id,
-      url: `${photo.prefix}original${photo.suffix}`,
-    }));
-
-    const tips = data.tips.map((tip: any) => ({
-      id: tip.id,
-      text: tip.text,
-    }));
-
-    return (
-      <div className={styles["page"]}>
-        <Container centerContent>
-          <div className={styles["page__flex"]}>
-            <Gallery images={images} />
-            <div className={styles["page__container"]}>
-              <div className={styles["page__content"]}>
-                <div className={styles["page__header"]}>
-                  <Link href="/">Go Back</Link>
-                  <Text size="2xl" weight="semibold">
-                    {data.details.name}
-                  </Text>
-                  <Text size="sm">
-                    {data.details.location.formatted_address}
-                  </Text>
-                </div>
-                <div className={styles["page__comments"]}>
-                  <FixedSizeList
-                    height={200}
-                    width="100%"
-                    itemCount={tips.length}
-                    itemSize={100}
-                  >
-                    {({ index, style }) => (
-                      <div style={style}>
-                        <CommentCard>{tips[index].text}</CommentCard>
-                      </div>
-                    )}
-                  </FixedSizeList>
-                </div>
-              </div>
-              <aside className={styles["page__aside"]}>
-                <iframe
-                  src={`https://maps.google.com/maps?q=${data.details.geocodes.main.latitude},${data.details.geocodes.main.longitude}&z=18&ie=UTF8&output=embed`}
-                  className={styles["page__map"]}
-                ></iframe>
-              </aside>
-            </div>
-          </div>
-        </Container>
-      </div>
-    );
-  }
-
-  return <></>;
 };
 
 export default IndexPage;
