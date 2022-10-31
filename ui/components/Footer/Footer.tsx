31efe819fa8d1975fcb263736087397c52255664
Marjo Forcado <marjoforcado@gmail.com>
2022-10-31 09:02:42 +0800

    Add footer

--- ui/components/Footer/Footer.tsx
+++ ui/components/Footer/Footer.tsx
@@ -1,3 +1,15 @@
-const Footer = () => <footer>footee</footer>;
+import Brand from "../Brand";
+import Container from "../Container";
+import Text from "../Text";
+import styles from "./styles.module.scss";
+
+const Footer = () => (
+  <footer className={styles["footer"]}>
+    <Container className={styles["footer__container"]}>
+      <Brand />
+      <Text>Coded by Marjo Forcado</Text>
+    </Container>
+  </footer>
+);
 
 export default Footer;
