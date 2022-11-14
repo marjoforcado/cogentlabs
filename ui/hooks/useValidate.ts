388e6438413e54f6ed11ab10440bc392da6e7a01
Marjo Forcado <marjoforcado@gmail.com>
2022-11-14 17:27:14 +0800

    Implement regex

--- ui/hooks/useValidate.ts
+++ ui/hooks/useValidate.ts
@@ -4,7 +4,9 @@ const useValidate = (form: any) => {
   const [errorMessage, setErrorMessage] = useState("");
 
   useEffect(() => {
-    if (!!+form.search || form.search.trim() === "") {
+    if (/(.+)@(.+){2,}\.(.+){2,}/.test(form.search.trim())) {
+      setErrorMessage("Email is not allowed.");
+    } else if (/^\d+$/.test(form.search.trim()) || form.search.trim() === "") {
       setErrorMessage("Value must be a valid string.");
     } else {
       setErrorMessage("");
